8b95ca26bc14064179267e9b6aec68e4097bbf58
Lais <laismassacesi@gmail.com>
2022-02-16 09:21:00 -0300

    Adiciona quadrados de pixel

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const pixelBoard = document.querySelector('#pixel-board');
+const pixel = document.querySelector('.pixel');
+
+for(let i = 0; i <= 25; i += 1){
+   let babyDiv = document.createElement('div');
+   babyDiv.className = 'pixel';
+   pixelBoard.appendChild(babyDiv);
+}
 
