70c3b2b330e7810acd17bb724b27c4ea35032ce4
Lais <laismassacesi@gmail.com>
2022-02-24 12:11:52 -0300

    Adiciona requisito 7 e 8

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ for(let i = 0; i < 25; i += 1){
    pixelBoard.appendChild(pixel);
 }
 
-
-
-
 function selectColorPixel(){
 const colorPalette = document.querySelectorAll('.color');
 console.log('colorPalette:' + colorPalette);
@@ -19,6 +16,7 @@ for(let i = 0; i < colorPalette.length; i += 1){
    colorPalette[i].addEventListener('click', manegerSelected);
    }
 }
+selectColorPixel();
 
 function manegerSelected(event){
    const selected = document.querySelector('.selected'); 
@@ -27,121 +25,13 @@ function manegerSelected(event){
    console.log('select.classList:' + selected.classList);
    event.target.classList.add('selected');
    console.log('event.target.classList:' + event.target.classList);
-   // let color = window.getComputedStyle(event.target);
-   // event.target.style.backgroundColor(selected);
 }
 
-
-   
-   //função que adiciona a classe color ao pixel clicado
-function  selectPixel(event){
-   const selected = document.querySelector('.selected');
-   const color = selected.style.backgroundColor;
-   console.log('color: ' + color)
-   event.target.classList.add('color')
-}
 
 pixelBoard.addEventListener('click', function (event) {           
    const selected = document.querySelector('.selected');
    console.log('selected: ' + selected);
-   let cor = window.getComputedStyle(selected);
-   event.target.style.backgroundColor = cor.getPropertyValue('background-color')
+   let getStyleSelected = window.getComputedStyle(selected);
+   event.target.style.backgroundColor = getStyleSelected.getPropertyValue('background-color')
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-// function paint (event){
-//    const pixelBoard = document.querySelectorAll('#pixel-board')
-//    pixelBoard.addEventListener('click', function(){
-//    const cor = window.getComputedStyle(selectedItemColor);
-//    event.target.style.backgroundColor = selectedItemColor.getPropertyValue('background-color'); 
-//    })
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function getColor (event){
-//    const selectPixel = event;
-//    for(let i = 0; i < pixels; i += 1){
-//    if(pixels[i].className === 'selected'){
-//       pixels[i].className = 'color'
-//    }
-//  }
-//  getColor.target.className = 'selected'
-// }
-
-
-
-// black.style.backgroundColor = "black";
-
-// window.onload = function(){
-// document.querySelector('.color').classList.add('selected')
-// };
-
-
-
-
-// function blackPixel (){
-//       const black = document.querySelector('.black')
-//          pixel.classList.add(black)
-// }
-// blackPixel()
-
-
-
-
-// function selectBlack (event){
-
-//       if(selected){
-//         selected.classList.remove('selected');
-//       }
-//       event.target.classList.add('selected');
-// }
-
-
-
-// const selectColor = document.getElementsByClassName('selected');
-//    black.classList.add('selected');
-//    black.style.backgroundColor = "black";
-//    for(let i in selectColor){
-//       if(selectColor[i] !== black){
-//          selectColor[i].classList.remove('selected');
-//       }
-//    }
-
-
-
-// function blackPixel (){
-//    const selected = document.querySelector('.selected')
-//    const black = document.querySelector('.black')
-//    if(black){
-//               selected.target.classList.remove('.black');
-//             }
-//             selected.target.classList.add('.black');
-// }
-// blackPixel()
