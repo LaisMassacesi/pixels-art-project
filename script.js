715ea3abdb9db97098f26f4128a01b8a13ff345a
Lais <laismassacesi@gmail.com>
2023-03-30 20:44:05 -0300

    refatoração e correção de erros lint

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
-const pixelBoard = document.querySelector('#pixel-board')
+const pixelBoard = document.querySelector('#pixel-board');
 
+function generatePixelBoard() {
+  for (let i = 0; i < 30; i += 1) {
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    pixelBoard.appendChild(pixel);
+  }
+}
+generatePixelBoard();
 
-
-for(let i = 0; i < 25; i += 1){
-   let pixel = document.createElement('div');
-   pixel.className = 'pixel';
-   pixelBoard.appendChild(pixel);
+function manegerSelected(event) {
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  event.target.classList.add('selected');
 }
 
-function selectColorPixel(){
-const colorPalette = document.querySelectorAll('.color');
-console.log('colorPalette:' + colorPalette);
-for(let i = 0; i < colorPalette.length; i += 1){
-   console.log('entrei no for');
-   colorPalette[i].addEventListener('click', manegerSelected);
-   }
+function selectColorPixel() {
+  const colorPalette = document.querySelectorAll('.color');
+  for (let i = 0; i < colorPalette.length; i += 1) {
+    console.log('entrei no for');
+    colorPalette[i].addEventListener('click', manegerSelected);
+  }
 }
 selectColorPixel();
 
-function manegerSelected(event){
-   const selected = document.querySelector('.selected'); 
-   console.log('selected: ' + selected);
-   selected.classList.remove('selected');
-   console.log('select.classList:' + selected.classList);
-   event.target.classList.add('selected');
-   console.log('event.target.classList:' + event.target.classList);
-}
-
-
-pixelBoard.addEventListener('click', function (event) {           
-   const selected = document.querySelector('.selected');
-   console.log('selected: ' + selected);
-   let getStyleSelected = window.getComputedStyle(selected);
-   event.target.style.backgroundColor = getStyleSelected.getPropertyValue('background-color')
-})
-
+pixelBoard.addEventListener('click', (event) => {
+  const selected = document.querySelector('.selected');
+  const getStyleSelected = window.getComputedStyle(selected);
+  event.target.style.backgroundColor = getStyleSelected.getPropertyValue('background-color');
+});
